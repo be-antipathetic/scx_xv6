d1e37176d44d3a54c0385200bcfdb07edb58c268
sanshui_linux <sanshui@DESKTOP-7DOERLU>
2025-01-08 19:27:54 +0800

    注释了物理内存分页相关代码

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,6 +14,8 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
+// 对于 run 这个结构体比较抽象， struct run * r; *r 中存放的是当前页面页首地址
+// 而 *(r->next) 则存放的是下一个页面的页首地址
 struct run {
   struct run *next;
 };
@@ -27,6 +29,7 @@ void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  // PHYSTOP 
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -34,8 +37,10 @@ void
 freerange(void *pa_start, void *pa_end)
 {
   char *p;
+  //将pa_start地址向上对其到页面大小的整数倍,p会指向该起始地址之后最近的一个页起始位置
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    //将该页内存添加到空闲内存链表中
     kfree(p);
 }
 
@@ -47,6 +52,7 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -54,6 +60,7 @@ kfree(void *pa)
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
+  //将pa类型转换为 struct run*， pa 指向每页内存页首位置
   r = (struct run*)pa;
 
   acquire(&kmem.lock);
